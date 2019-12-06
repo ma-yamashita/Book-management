91d186a6df69dc494522ca3567addc5be9255f79
ma-yamashita <mariko@esm.co.jp>
2019-12-06 17:34:33 +0900

    初期画面の作成

--- GAS/main.js
+++ GAS/main.js
@@ -1,8 +1,24 @@
 function doGet() {
-//  return HtmlService.createTemplateFromFile("GAS/list").evaluate();
-  return HtmlService.createTemplateFromFile("GAS/regist").evaluate();
+  return HtmlService.createTemplateFromFile("GAS/list").evaluate();
+//  return HtmlService.createTemplateFromFile("GAS/regist").evaluate();
 }
   
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function getData() {
+  return SpreadsheetApp
+    .openById('1JeDvyRevWXoVMOora4br1EvkwhE8hhp10-OkBxUdI1o')
+    .getActiveSheet()
+    .getDataRange()
+    .getValues();
+}
+
+function fomatdate(mydate) {
+  var fdate = "";
+  if (typeof mydate == "object") {
+    fdate = Utilities.formatDate(new Date(mydate), "JST", "yyyy/MM/dd");
+  }
+  return fdate; 
 }
