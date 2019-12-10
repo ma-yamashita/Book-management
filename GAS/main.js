6ca3670dcc1ca3a3b58d3d2443f30e6f9b3cf5a4
ma-yamashita <mariko@esm.co.jp>
2019-12-10 09:43:13 +0900

    検索条件クリアボタン作成

--- GAS/main.js
+++ GAS/main.js
@@ -1,6 +1,6 @@
 function doGet() {
-//  return HtmlService.createTemplateFromFile("GAS/list").evaluate();
-  return HtmlService.createTemplateFromFile("GAS/regist").evaluate();
+  return HtmlService.createTemplateFromFile("GAS/list").evaluate();
+//  return HtmlService.createTemplateFromFile("GAS/regist").evaluate();
 }
   
 function include(filename) {
