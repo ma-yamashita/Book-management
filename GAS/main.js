3fca93f61ec48b464a8d9c4a7f5bd09dcf3ce183
ma-yamashita <mariko@esm.co.jp>
2019-12-12 08:37:33 +0900

    検索ボタン作成

--- GAS/main.js
+++ GAS/main.js
@@ -7,49 +7,32 @@ function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
-// function getData() {
-//   return SpreadsheetApp
-//     .openById('1JeDvyRevWXoVMOora4br1EvkwhE8hhp10-OkBxUdI1o')
-//     .getActiveSheet()
-//     .getDataRange()
-//     .getValues();
-// }
-
 function mytest() {
   getData("HTTP");
 }
 
-function getData(paramete1,paramete2) {
-  
-  var sheet = SpreadsheetApp.openById('1JeDvyRevWXoVMOora4br1EvkwhE8hhp10-OkBxUdI1o').getActiveSheet();
-  const master = sheet.getDataRange().getValues();
-  
-  if (paramete1===undefined) {
-    return master;
-  } else {
-    Logger.log(paramete1);
-    Logger.log(paramete2);
-    //部分一致？
-    var re = new RegExp('/\*' + paramete1 + '\*/');
-    master.filter(function(e){
-      Logger.log(re);
-      Logger.log(re.test(e[0]))});
+function getData(p1, p2) {
+  var ssId = '1JeDvyRevWXoVMOora4br1EvkwhE8hhp10-OkBxUdI1o';
+  var ss = SpreadsheetApp.openById(ssId);
+  var sheet = ss.getSheetByName('books');
+  var values = sheet.getDataRange().getValues();
+  // テキストボックスの値が未入力であった場合
+  Logger.log("p1:" + p1);
+  Logger.log("p2:" + p2);
+  if (!p1) return values;
 
-    
-    const extraction = master
-    .filter(function(e){return re.test(e[0])})
-    .map(function(e){
-      const columns = [0,1,2,3,4,5,6],row = [];
-      for (var i = 0; i < columns.length; i++) row.push(e[columns[i]]);
-      return row;
-    });
-    
-    Logger.log("test");
-    Logger.log(extraction.length);
-    Logger.log(extraction);
-    return extraction;//再描画？
-  }  
-}
+  var result = [];
+  var re = new RegExp('\.*' + p1 + '\.*');
+  var head = values.shift();
+  result.push(head);
+
+  Logger.log(re);
+  Logger.log(re.test(values[0]));
+
+  values.forEach(function(value) {
+    if (re.test(value[0])) result.push(value);
+  });
+  return result;
 
 function fomatdate(mydate) {
   var fdate = "";
